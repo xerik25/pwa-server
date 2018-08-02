c21a80d3cb25cc9caf2c83005be80055f60b329f
cliff <cbarendsen@signet.tv>
2018-08-01 22:46:42 -0700

    updated package and logic to run react start not build

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const router = express.Router()
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 3000
 server.listen(port)
 
 app.use('/', router)
